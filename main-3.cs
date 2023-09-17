d518692f18b6069291b916e4d05a9227f2362e56
Guefront Vernard <142953187+Epson2@users.noreply.github.com>
2023-09-16 23:20:19 -0400

    Add files via upload

---
+++ main-3.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+
+class Program {
+  public static void Main (string[] args) {
+    WriteLine ("Oh-oh-oh-oh-oh,");
+    WriteLine ("oh-oh-oh-oh");
+    WriteLine ("oh-oh-oh / ");
+    WriteLine ("Caught in a bad romance...");
+  }
+}
